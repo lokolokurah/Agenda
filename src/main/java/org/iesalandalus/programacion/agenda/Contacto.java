abeda740ca46bc53f3fd4035f9ef2bdab7e3747a
Jaime <Jaime@HYRULE-5500X>
2018-12-10 10:06:20 +0100

    Creada Clase Contacto + Atributos

---
+++ src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.agenda;
+
+/**
+ *
+ * @author Jaime
+ */
+//Clase contacto + Atributos
+public class Contacto 
+{
+    private static final String ER_TELEFONO="";
+    private static final String ER_CORREO="";
+    private String nombre;
+    private String telefono;
+    private String correo;
+}
