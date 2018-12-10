7e8229cc6e18b8e7ce280366ff3e3546d251747a
Jaime Ruiz Ramirez <>
2018-12-10 12:05:17 +0100

    Creados los métodos get y set.  Utilizados dos atributos de clase que no se puedan modificar para guardar las expresiones regulares a validar.

--- src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
@@ -5,6 +5,8 @@
  */
 package org.iesalandalus.programacion.agenda;
 
+import java.util.regex.Pattern;
+
 /**
  *
  * @author Jaime
@@ -12,9 +14,69 @@ package org.iesalandalus.programacion.agenda;
 //Clase contacto + Atributos
 public class Contacto 
 {
-    private static final String ER_TELEFONO="";
-    private static final String ER_CORREO="";
+    private static final String ER_TELEFONO="[69][0-9]{8}";
+    private static final String ER_CORREO="^[_A-Za-z0-9-\\\\+]+(\\\\.[_A-Za-z0-9-]+)*@\" + \"[A-Za-z0-9-]+(\\\\.[A-Za-z0-9]+)*(\\\\.[A-Za-z]{2,4})$";
     private String nombre;
     private String telefono;
     private String correo;
+   
+     public String getNombre() {
+        return nombre;
+    }
+     
+     private void setNombre(String nombre) {
+        
+        boolean contactoCreado = false;
+        if (this.nombre != null) {
+            contactoCreado = true;
+        }
+        if (nombre != null && nombre.equals("") && contactoCreado != true) {
+            this.nombre = nombre;
+        } else {
+            throw new IllegalArgumentException(" El nombre un contacto no puede ser nulo o vacío. ");
+        }
+    }
+     
+     public String getTelefono() {
+        return telefono;
+    }
+     
+     public void setTelefono(String telefono) {
+        
+        if (telefono != null && telefono.equals(""))
+            if(Pattern.matches(ER_TELEFONO, telefono))
+            {
+                this.telefono = telefono;
+            }else
+            {
+            throw new IllegalArgumentException(" El teléfono de un contacto no puede ser nulo o vacío. ");
+            }   
+        else 
+        {
+        throw new IllegalArgumentException(" El teléfono no tiene un formato válido. ");
+        }
+        
+    }
+     public String getCorreo() {
+        return correo;
+    }
+     
+     public void setCorreo(String correo) {
+         /*Para la validación de correo electronico voy usar  el método estático 
+         compile de la clase Pattern que permite crear expresión 
+         regular o patrón.*/
+             if (correo != null && correo.equals("")) 
+                if (Pattern.matches(ER_CORREO, correo))
+                {
+                this.correo = correo;
+                } 
+                else 
+                {
+                throw new IllegalArgumentException(" El correo no tiene un formato válido. ");
+                }
+            else 
+            {
+            throw new IllegalArgumentException(" El teléfono no tiene un formato válido. ");
+            }
+        } 
 }
