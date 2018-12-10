00e053dee16e3f17d77b2df246aec2c524ab5589
Jaime Ruiz Ramirez <Jaime@HYRULE-5500X>
2018-12-10 19:00:28 +0100

    Constructor y métodos get de la clase Agenda

--- src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
@@ -12,6 +12,30 @@ package org.iesalandalus.programacion.agenda;
 //Creada la clase Agenda + Atributos
 public class Agenda 
 {
-    private static final int MAX_CONTACTOS = 0;
-    private int numContactos = 0; //Lo inicializo para la hora en la que cree el constructor copia
+    private static final String MENSAJE_EXCEPCION = " Debería haber saltado la excepción. ";
+    private static final String MENSAJE_NO_EXCEPCION = " No debería haber saltado la excepción. ";
+    
+    private static final int MAX_CONTACTOS = 0; //Lo inicializo para la hora en la que cree el constructor copia
+    private int numContactos; 
+    private Contacto [] contacto;
+    
+    public Agenda(int numContactos)
+    {        
+        if (numContactos>0)
+        {
+            this.numContactos = numContactos;
+         }
+        else
+            throw new IllegalArgumentException(" ERROR: Ha introducido un número de páginas no válido ");
+        }
+    
+     public int getNumContactos() {
+        return numContactos;
+    }
+     
+     //Get que devuelve una copia del array de contactos
+    public Contacto[] getContacto() {
+        return this.contacto;
+    }
+
 }
