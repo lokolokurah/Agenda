1788b44cc8c0321a035dc599e1ae24d9d7a970cd
Jaime Ruiz Ramirez <Jaime@HYRULE-5500X>
2018-12-10 19:22:38 +0100

    Creado método añadir + Correción en declaraciones

--- src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Agenda.java
@@ -5,6 +5,8 @@
  */
 package org.iesalandalus.programacion.agenda;
 
+import javax.naming.OperationNotSupportedException;
+
 /**
  *
  * @author Jaime
@@ -15,9 +17,60 @@ public class Agenda
     private static final String MENSAJE_EXCEPCION = " Debería haber saltado la excepción. ";
     private static final String MENSAJE_NO_EXCEPCION = " No debería haber saltado la excepción. ";
     
-    private static final int MAX_CONTACTOS = 0; //Lo inicializo para la hora en la que cree el constructor copia
+    private static final int MAX_CONTACTOS = 5; 
     private int numContactos; 
-    private Contacto [] contacto;
+    private Contacto [] contactos;
+    
+    //P.dfcto
+     public Agenda()
+    {
+        contactos = new Contacto[MAX_CONTACTOS];
+    }
+     
+     public void añadir(Contacto contacto )
+        {
+             int indice;
+             try {
+                indice = buscarPrimerIndiceComprobandoExistencia(contacto);
+                if(indiceNoSuperaTamano(indice)) {
+                    this.contactos[indice] = contacto;
+                } else {
+                    System.out.println(" El array está lleno ");
+                }
+                } catch (OperationNotSupportedException e) {
+                    System.out.println(" Operación  no soportada ");
+                    e.getMessage();
+                }   
+        }
+            
+        private int buscarPrimerIndiceComprobandoExistencia(Contacto contacto) throws OperationNotSupportedException
+        {
+            int indice=0;
+            boolean encontradoContacto=false;
+             for(int i=0;i<contactos.length && !encontradoContacto;i++)
+            {
+                if (contactos[i]==null)
+                {
+                    encontradoContacto=true;
+                    indice = i;
+                }
+                else if (contactos[i].equals(contacto))
+                {
+                    throw new OperationNotSupportedException(" Ya existe un contacto con ese nombre. ");
+                }               
+            }
+             return indice;
+        }
+        
+         private boolean indiceNoSuperaTamano(int i)
+        {
+            if (i<MAX_CONTACTOS)
+                return true;
+           else
+                return false;
+            
+        }
+        
     
     public Agenda(int numContactos)
     {        
@@ -35,7 +88,7 @@ public class Agenda
      
      //Get que devuelve una copia del array de contactos
     public Contacto[] getContacto() {
-        return this.contacto;
+        return this.contactos;
     }
 
 }
